21b3ef4a468b630105eac8aad31afc3d5ab90fa5
climousin <>
2021-03-09 18:13:14 +0100

    rectification du domaine

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(filrouge.cli.p.2021.ajoga.fr)
+domains=(filrouge.cli.p2021.ajoga.fr)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="clement.limousin@student-cs.fr" # Adding a valid address is strongly recommended
